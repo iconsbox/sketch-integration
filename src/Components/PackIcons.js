477762e12de8f38d34df03fecb25016d93f4e5f0
jafarRezaei <bomber.man87@yahoo.com>
2020-09-17 15:46:21 +0430

    fix indent

--- src/Components/PackIcons.js
+++ src/Components/PackIcons.js
@@ -2,17 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const PackIcons = ({ icons }) => Object.keys(icons).map(icon => {
-    const Icon = icons[icon];
-    return (
-        <Icon
-            style={{
-                margin: 12,
-                borderRadius: 5,
-            }} stroke="#fff" name={icon} width={30} height={30} key={icon} />
-    )});
+  const Icon = icons[icon];
+  return (
+    <Icon
+      style={{
+        margin: 12,
+        borderRadius: 5,
+      }} name={icon} width={30} height={30} key={icon} />
+  )});
 
 PackIcons.propTypes = {
-    icons: PropTypes.object,
+  icons: PropTypes.object,
 };
 
 export default PackIcons;
